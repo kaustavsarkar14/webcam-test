10115b40e7ef9871495c545196b8f0bf66705fd0
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-08-31 21:56:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
+import Webcam from 'react-webcam';
 import './App.css';
+import { exportComponentAsJPEG } from 'react-component-export-image';
 
 function App() {
+  const webcamRef = useRef(null);
+  const imageRef = useRef(null);
+  const [imgURL, setImgURL] = useState(null)
+  const [buttonText, setButtonText] = useState("Click Photo")
+  const handleClick =()=>{
+    if(!imgURL){
+      setImgURL(webcamRef.current.getScreenshot())
+      setButtonText("Retake")
+    }
+    else{
+      setImgURL(null)
+      setButtonText("Click Photo")
+    }
+  }
+  const downloadImage=()=>{
+    const fileName = 'custom_image_name.jpg';
+    exportComponentAsJPEG(imageRef, fileName);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        !imgURL?
+         <Webcam ref={webcamRef} />
+        :
+        <img ref={imageRef} src={imgURL} alt="" />
+      }
+      <button onClick={handleClick} >{buttonText}</button>
+      {imgURL && <button className='download' onClick={downloadImage} >Download Image</button>}
     </div>
   );
 }
